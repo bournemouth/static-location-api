ae738d01a97cb87f0478f4dec61a488d94cbb316
Chris Sherry <chris@chrissherry.co.uk>
2015-02-07 15:55:21 +0000

    fix stupid commit

--- src/TaxiRank/TaxiRankController.php
+++ src/TaxiRank/TaxiRankController.php
@@ -2,23 +2,48 @@
 
 namespace BournemouthData\TaxiRank;
 
-use BournemouthData\ResourceController;
-use BournemouthData\TransportRepositoryInterface;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Response;
 
-class TaxiRankController extends ResourceController {
+class TaxiRankController {
 
     /**
      * @param Application $app
-     * @param TransportRepositoryInterface $repository
-     * @param $collectionName
-     * @param $baseRoute
      */
-    public function __construct(Application $app, TransportRepositoryInterface $repository, $collectionName, $baseRoute)
+    public function __construct(Application $app)
     {
-        parent::__construct($app, $repository, $collectionName, $baseRoute);
+        $this->app = $app;
     }
 
+
+    public function getAll()
+    {
+        $taxiRankRecords = $this->app['db.taxiRanks'];
+
+        $taxiRankCollection = new \Nocarrier\Hal('/api/v1/taxi-ranks');
+
+        foreach ($taxiRankRecords as $taxiRank) {
+            $taxiRankResource = new \Nocarrier\Hal('/api/v1/taxi-ranks/' . $taxiRank['id'], $taxiRank);
+            $taxiRankCollection->addResource('taxiRanks', $taxiRankResource);
+        }
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/hal+json');
+        $response->setContent($taxiRankCollection->asJson(true));
+        return $response;
+    }
+
+    public function getTaxiRank()
+    {
+        $id = $this->app['request']->get('id');
+        $taxiRank = $this->app['db.taxiRanks'][$id];
+
+        $taxiRankResource = new \Nocarrier\Hal('/api/v1/taxi-ranks/' . $taxiRank['id'], $taxiRank);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/hal+json');
+        $response->setContent($taxiRankResource->asJson(true));
+        return $response;
+    }
 
 }
